e0750bec0fed4677cae9921a07a0b6b925caec9e
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-14 18:08:54 -0300

    Categorias Modificando

--- app/api/mensura/router.py
+++ app/api/mensura/router.py
@@ -3,7 +3,7 @@
 from fastapi import APIRouter, Depends
 
 from app.api.mensura.controllers.categoriasDeliveryController import router as routerCategoriasDelivery
-from app.api.mensura.controllers.produtosDeliveryController import produtosDeliveryRouter
+from app.api.mensura.controllers.produtosDeliveryController import router as routerProdutosDelivery
 from app.api.mensura.controllers.cardapioController import router as routerCardapio
 from app.api.mensura.controllers.subCategoriasController import router as routerSubCategorias
 from app.core.dependencies import get_current_user
@@ -11,6 +11,6 @@ from app.core.dependencies import get_current_user
 router = APIRouter()
 
 router.include_router(routerCardapio)
-router.include_router(produtosDeliveryRouter, dependencies=[Depends(get_current_user)])
+router.include_router(routerProdutosDelivery, dependencies=[Depends(get_current_user)])
 router.include_router(routerCategoriasDelivery, dependencies=[Depends(get_current_user)])
 router.include_router(routerSubCategorias, dependencies=[Depends(get_current_user)])
