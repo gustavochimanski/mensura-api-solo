b771b83da613b8f47ef93d972039e267668d332a
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-22 13:07:17 -0300

    .

--- app/api/mensura/controllers/cardapioController.py
+++ app/api/mensura/controllers/cardapioController.py
@@ -3,15 +3,16 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.api.mensura.repositories.cardapio.CardapioRepository import CardapioRepository
-from app.api.mensura.schemas.delivery.cardapio.cardapio_schema import ProdutoEmpMiniDTO, CardapioCategProdutosResponse, \
+from app.api.mensura.schemas.delivery.cardapio.cardapio_schema import  \
     VitrineComProdutosResponse
+from app.api.mensura.schemas.delivery.categorias.categoria_schema import CategoriaDeliveryOut
 from app.api.mensura.services.CardapioService import CardapioService
 from app.api.mensura.services.ProdutosDeliveryService import ProdutosDeliveryService
 from app.database.db_connection import get_db
 
 router = APIRouter(tags=["Cardapio"])
 
-@router.get("/cardapio", response_model=List[CardapioCategProdutosResponse])
+@router.get("/cardapio", response_model=List[CategoriaDeliveryOut])
 def listar_cardapio(cod_empresa: int = 1, db: Session = Depends(get_db)):
     repo = CardapioRepository(db)
     service = CardapioService(repo)
