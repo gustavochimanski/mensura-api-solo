a3f3f28e8b919d7edf392d276168411ef1c2db5e
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-25 18:24:56 -0300

    SEPARAÇÃO DE IS_HOME

--- app/api/mensura/controllers/cardapioController.py
+++ app/api/mensura/controllers/cardapioController.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
-from typing import List, Union
+from typing import List
 
 from app.api.mensura.schemas.delivery.cardapio.cardapio_schema import  \
     VitrineComProdutosResponse
@@ -10,25 +10,25 @@ from app.database.db_connection import get_db
 
 router = APIRouter(tags=["Cardapio"])
 
-@router.get("/cardapio", response_model=Union[List[CategoriaDeliveryOut], List[VitrineComProdutosResponse]])
+@router.get("/cardapio", response_model=List[CategoriaDeliveryOut])
 def listar_cardapio(
     empresa_id: int,
     db: Session = Depends(get_db),
-    is_home: bool = Query(...)
 ):
-    service = CardapioService(db, is_home)
-
-    if is_home:
-        return service.buscar_vitrines_home(empresa_id)
-
+    service = CardapioService(db)
     return service.listar_cardapio(empresa_id)
 
 
 @router.get("/produtos/vitrine-por-categoria", response_model=List[VitrineComProdutosResponse])
 def listar_vitrines_e_produtos_por_categoria(
     empresa_id: int = Query(...),
-    cod_categoria: int = Query(...),
-    db: Session = Depends(get_db)
+    cod_categoria: int = Query(0),
+    is_home: bool = Query(False),
+    db: Session = Depends(get_db),
 ):
     service = CardapioService(db)
+
+    if is_home:
+        return service.buscar_vitrines_home(empresa_id)
+
     return service.buscar_vitrines_com_produtos(empresa_id, cod_categoria)
