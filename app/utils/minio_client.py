699ebf55ff302a84f6d4e148ef9fb3593151c8e6
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-30 18:25:20 -0300

    Modelando DB

--- app/utils/minio_client.py
+++ app/utils/minio_client.py
@@ -22,7 +22,7 @@ MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER", "")
 MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD", "")
 
 client = Minio(
-    endpoint="localhost:1001",
+    endpoint="minio_global:1001",
     access_key=MINIO_ROOT_USER,
     secret_key=MINIO_ROOT_PASSWORD,
     secure=MINIO_ENDPOINT.startswith("https")
