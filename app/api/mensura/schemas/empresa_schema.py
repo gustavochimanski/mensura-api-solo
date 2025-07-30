e80fbec38e1a91adde964aa7687df7b935c4e87c
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-30 17:29:43 -0300

    Modelando DB

--- app/api/mensura/schemas/empresa_schema.py
+++ app/api/mensura/schemas/empresa_schema.py
@@ -1,27 +1,26 @@
+# empresa_schema.py
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
-
-from app.api.mensura.schemas.endereco_schema import EnderecoCreate
-
+from app.api.mensura.schemas.endereco_schema import EnderecoCreate, EnderecoResponse
 
 class EmpresaBase(BaseModel):
     nome: str
     cnpj: Optional[str] = None
     slug: str
     logo: Optional[str] = None
-    endereco: EnderecoCreate
 
 class EmpresaCreate(EmpresaBase):
-    pass
+    endereco: EnderecoCreate
 
 class EmpresaUpdate(BaseModel):
     nome: Optional[str] = None
     cnpj: Optional[str] = None
     slug: Optional[str] = None
     logo: Optional[str] = None
-    endereco: EnderecoCreate
+    endereco_id: Optional[int] = None  # ou endereco: EnderecoUpdate se quiser editar inline
 
 class EmpresaResponse(EmpresaBase):
     id: int
+    endereco: Optional[EnderecoResponse] = None
 
     model_config = ConfigDict(from_attributes=True)
