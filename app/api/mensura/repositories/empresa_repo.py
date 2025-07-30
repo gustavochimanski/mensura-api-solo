6d5462e3abae97da9012540a574a4b8651a1e711
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-07-30 17:34:18 -0300

    Modelando DB

--- app/api/mensura/repositories/empresa_repo.py
+++ app/api/mensura/repositories/empresa_repo.py
@@ -7,7 +7,9 @@ class EmpresaRepository:
         self.db = db
 
     def get_empresa_by_id(self, empresa_id: int) -> Optional[EmpresaModel]:
-        return self.db.query(EmpresaModel).filter(EmpresaModel.id == empresa_id).first()
+        return (self.db.query(EmpresaModel)
+                .options(joinedload(EmpresaModel.endereco))
+                .filter(EmpresaModel.id == empresa_id).first())
 
     def get_cnpj_by_id(self, emp_id: int) -> Optional[str]:
         return self.db.query(EmpresaModel.cnpj).filter(EmpresaModel.id == emp_id).scalar()
