b37ca3aba883f457d4ba78d059376ea391c88004
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-08-01 16:00:59 -0300

    IMPLEMENTADNO MEIO DE PAGAMENTO NO DASHBOARD

--- app/api/BI/repositories/meio_pagamento_repo.py
+++ app/api/BI/repositories/meio_pagamento_repo.py
@@ -3,8 +3,8 @@ from decimal import Decimal
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-from app.api.public.models.meiospagamento import MeiosPgtoPublicModel
 from app.api.pdv.models.meio_pagamento.movmeiopgto_pdv import MovMeioPgtoPDVModel
+from app.api.public.models.meiospgto_public import MeiosPgtoPublicModel
 
 
 class MeioPagamentoRepository:
