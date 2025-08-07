8e14f41f4a60d40672fb72865e8a3dbd6692966e
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-08-07 17:42:58 -0300

    Adcionando pedido

--- app/api/delivery/router.py
+++ app/api/delivery/router.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter, Depends
 
 from app.api.delivery.controllers.cardapio_dv_controller import router as router_cardapio
 from app.api.delivery.controllers.cliente_controller import router as router_clientes
+from app.api.delivery.controllers.pedidos_dv_controller import router as router_pedidos
 from app.api.delivery.controllers.categorias_dv_controller import router as router_categorias
 from app.api.delivery.controllers.produtos_dv_controller import router as router_produtos
 from app.api.delivery.controllers.vitrine_controller import router as router_vitrines
@@ -13,6 +14,7 @@ router = APIRouter()
 
 router.include_router(router_cardapio)
 router.include_router(router_clientes)
+router.include_router(router_pedidos)
 router.include_router(router_categorias, dependencies=[Depends(get_current_user)])
 router.include_router(router_produtos, dependencies=[Depends(get_current_user)])
 router.include_router(router_vitrines, dependencies=[Depends(get_current_user)])
--- app/api/delivery/schemas/cliente_schema.py
+++ app/api/delivery/schemas/cliente_schema.py
@@ -1,16 +1,16 @@
 # src/app/api/delivery/schemas/cliente_schemas.py
 from datetime import date
+from typing import Optional
 from pydantic import BaseModel, EmailStr, constr, ConfigDict
 
-
-# Campos que vêm na resposta
+# Resposta para o cliente
 class ClienteOut(BaseModel):
     id: int
-    nome: constr(min_length=1, max_length=100)
-    cpf: constr(max_length=14) | None
-    telefone: constr(max_length=20) | None
-    email: EmailStr | None
-    data_nascimento: date | None
+    nome: str
+    cpf: Optional[str]
+    telefone: str
+    email: Optional[EmailStr]
+    data_nascimento: Optional[date]
     ativo: bool
 
     model_config = ConfigDict(from_attributes=True)
@@ -18,16 +18,16 @@ class ClienteOut(BaseModel):
 # Campos para criação
 class ClienteCreate(BaseModel):
     nome: constr(min_length=1, max_length=100)
-    cpf: constr(max_length=14) | None = None
-    telefone: constr(max_length=20) | None = None
-    email: EmailStr | None = None
-    data_nascimento: date | None = None
+    cpf: Optional[constr(max_length=14)] = None
+    telefone: Optional[constr(max_length=20)] = None
+    email: Optional[EmailStr] = None
+    data_nascimento: Optional[date] = None
 
 # Campos para atualização parcial
 class ClienteUpdate(BaseModel):
-    nome: constr(min_length=1, max_length=100) | None = None
-    cpf: constr(max_length=14) | None = None
-    telefone: constr(max_length=20) | None = None
-    email: EmailStr | None = None
-    data_nascimento: date | None = None
-    ativo: bool | None = None
+    nome: Optional[constr(min_length=1, max_length=100)] = None
+    cpf: Optional[constr(max_length=14)] = None
+    telefone: Optional[constr(max_length=20)] = None
+    email: Optional[EmailStr] = None
+    data_nascimento: Optional[date] = None
+    ativo: Optional[bool] = None
