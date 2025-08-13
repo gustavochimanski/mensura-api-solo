f6e7955175137049ced55b7a43bae6b641a2dbcb
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-08-13 12:42:55 -0300

    Implementando nova query meio pagamento

--- app/api/public/controllers/empresas/getEmpresasController.py
+++ app/api/public/controllers/empresas/getEmpresasController.py
@@ -5,15 +5,15 @@ from sqlalchemy.orm import Session
 from app.database.db_connection import get_db
 from app.api.public.repositories.empresas.consultaEmpresas import EmpresasRepository
 
-router = APIRouter(prefix="/api/public/empresas", tags=["Public"])
+router = APIRouter( tags=["Public"])
 logger = logging.getLogger(__name__)
 
-@router.get("", summary="Buscar códigos das empresas ativas")
+@router.get("/api/public/empresas", summary="Buscar códigos das empresas ativas")
 def get_empresas_codigos(db: Session = Depends(get_db)):
     repo = EmpresasRepository(db)
     return repo.buscar_codigos_ativos()
 
-@router.get("/detalhes", summary="Buscar objetos completos das empresas ativas")
+@router.get("/api/public/empresas/detalhes", summary="Buscar objetos completos das empresas ativas")
 def get_empresas_completas(db: Session = Depends(get_db)):
     repo = EmpresasRepository(db)
     return repo.buscar_empresas_ativas()
