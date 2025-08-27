aeeac96581e61cbc6425a555b2fc1e9820468450
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-08-27 18:38:28 -0300

    <

--- app/api/delivery/services/service_endereco_dv.py
+++ app/api/delivery/services/service_endereco_dv.py
@@ -1,4 +1,7 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
+
+from app.api.delivery.models.cliente_dv_model import ClienteDeliveryModel
 from app.api.delivery.repositories.repo_endereco_dv import EnderecoRepository
 from app.api.delivery.schemas.schema_endereco_dv import EnderecoOut, EnderecoCreate, EnderecoUpdate
 
@@ -8,19 +11,33 @@ class EnderecosService:
         self.repo = EnderecoRepository(db)
 
     def list(self, super_token: str):
-        return [EnderecoOut.model_validate(x) for x in self.repo.list_by_cliente(super_token)]
+        cliente_telefone = self._token_para_telefone(super_token)
+        return [EnderecoOut.model_validate(x) for x in self.repo.list_by_cliente(cliente_telefone)]
 
     def get(self, super_token: str, end_id: int):
-        return EnderecoOut.model_validate(self.repo.get_by_cliente(super_token, end_id))
+        cliente_telefone = self._token_para_telefone(super_token)
+        return EnderecoOut.model_validate(self.repo.get_by_cliente(cliente_telefone, end_id))
 
     def create(self, super_token: str, payload: EnderecoCreate):
-        return EnderecoOut.model_validate(self.repo.create(super_token, payload))
+        cliente_telefone = self._token_para_telefone(super_token)
+        return EnderecoOut.model_validate(self.repo.create(cliente_telefone, payload))
 
     def update(self, super_token: str, end_id: int, payload: EnderecoUpdate):
-        return EnderecoOut.model_validate(self.repo.update(super_token, end_id, payload))
+        cliente_telefone = self._token_para_telefone(super_token)
+        return EnderecoOut.model_validate(self.repo.update(cliente_telefone, end_id, payload))
 
     def delete(self, super_token: str, end_id: int):
-        self.repo.delete(super_token, end_id)
+        cliente_telefone = self._token_para_telefone(super_token)
+        self.repo.delete(cliente_telefone, end_id)
 
     def set_padrao(self, super_token: str, end_id: int):
-        return EnderecoOut.model_validate(self.repo.set_padrao(super_token, end_id))
+        cliente_telefone = self._token_para_telefone(super_token)
+        return EnderecoOut.model_validate(self.repo.set_padrao(cliente_telefone, end_id))
+
+    def _token_para_telefone(self, super_token: str) -> str:
+        from app.database.db_connection import get_db
+        db = self.db
+        cliente = db.query(ClienteDeliveryModel).filter_by(super_token=super_token).first()
+        if not cliente:
+            raise HTTPException(status_code=401, detail="Token inválido")
+        return cliente.telefone
