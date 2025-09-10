2402147b45deb71af607da6260d90ffd1bdc9bce
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-10 18:41:43 -0300

    .

--- app/api/mensura/router/router_geo_api_fy.py
+++ app/api/mensura/router/router_geo_api_fy.py
@@ -6,4 +6,4 @@ router = APIRouter(prefix="/api/mensura/geoapify", tags=["GEOAPIFY"])
 @router.get("/search-endereco")
 async def search_regiao(text: str):
     geo_api_fy = GeoapifyClient()
-    return await geo_api_fy.geocode_raw(text)
+    return await geo_api_fy.geocode_mini(text)
