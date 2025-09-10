2176acdae5e61fb01d1b0244b6c1793bce8ddd43
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-10 18:17:48 -0300

    .

--- app/api/mensura/router/router_geo_api_fy.py
+++ app/api/mensura/router/router_geo_api_fy.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 
 from app.utils.geopapify_client import GeoapifyClient
 
-router = APIRouter(prefix="/api/mensura/search-endereco-geoapify", tags=["Busca endereco por texto e retorna todos os campos"])
+router = APIRouter(prefix="/api/mensura/geoapify/search-endereco", tags=["GEOAPIFY"])
+
 def search_regiao(text: str):
     geo_api_fy = GeoapifyClient()
     return geo_api_fy.geocode_raw(text)
