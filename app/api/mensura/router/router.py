08a475a6537e2e2b5c8e7ddfe547411257eca049
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-10 19:10:18 -0300

    .

--- app/api/mensura/router/router.py
+++ app/api/mensura/router/router.py
@@ -3,6 +3,7 @@
 from fastapi import APIRouter
 
 from app.api.mensura.router.router_empresa import router as router_empresa
+from app.api.mensura.router.router_empresa_client import router as router_empresa_client
 from app.api.mensura.router.router_usuario import router as router_usuario
 from app.api.mensura.router.router_geo_api_fy import router as router_geoapify
 from app.api.mensura.router.router_endereco import router as router_endereco
@@ -10,6 +11,8 @@ from app.api.mensura.router.router_produtos import router as router_produtos
 
 router = APIRouter()
 router.include_router(router_empresa)
+router.include_router(router_empresa_client)
+
 router.include_router(router_endereco)
 router.include_router(router_geoapify)
 router.include_router(router_produtos)
