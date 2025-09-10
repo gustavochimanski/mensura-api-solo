f61e99ff04dd74e7cf4ed41df5280921a70650be
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-10 18:19:45 -0300

    .

--- app/api/mensura/router/router.py
+++ app/api/mensura/router/router.py
@@ -7,10 +7,10 @@ from app.api.mensura.router.router_usuario import router as router_usuario
 from app.api.mensura.router.router_geo_api_fy import router as router_geoapify
 from app.api.mensura.router.router_endereco import router as router_endereco
 from app.api.mensura.router.router_produtos import router as router_produtos
-router = APIRouter()
 
+router = APIRouter()
 router.include_router(router_empresa)
-router.include_router(router_usuario)
 router.include_router(router_endereco)
-router.include_router(router_produtos)
 router.include_router(router_geoapify)
+router.include_router(router_produtos)
+router.include_router(router_usuario)
--- app/api/mensura/router/router_geo_api_fy.py
+++ app/api/mensura/router/router_geo_api_fy.py
@@ -2,8 +2,9 @@ from fastapi import APIRouter
 
 from app.utils.geopapify_client import GeoapifyClient
 
-router = APIRouter(prefix="/api/mensura/geoapify/search-endereco", tags=["GEOAPIFY"])
+router = APIRouter(prefix="/api/mensura/geoapify", tags=["GEOAPIFY"])
 
+@router.get("/search-endereco")
 def search_regiao(text: str):
     geo_api_fy = GeoapifyClient()
     return geo_api_fy.geocode_raw(text)
