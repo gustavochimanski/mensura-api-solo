18ac0d6a19512e3aa466c359b4428cc22168a6d7
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-11 18:27:24 -0300

    .

--- app/api/delivery/services/pedidos/service_pedido.py
+++ app/api/delivery/services/pedidos/service_pedido.py
@@ -261,7 +261,6 @@ class PedidoService:
                     produto_descricao_snapshot=pe.produto.descricao if pe.produto else None,
                     produto_imagem_snapshot=pe.produto.imagem if pe.produto else None,
                 )
-            logger.info(f'CLIENTE_ID: {cliente_id}')
 
 
             desconto = self._aplicar_cupom(cupom_id=payload.cupom_id, subtotal=subtotal)
