cb2cc6b42b933fb2a1cc8ebca8c63bc86783e7f4
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-09-11 10:56:47 -0300

    .

--- app/api/delivery/schemas/schema_pedido.py
+++ app/api/delivery/schemas/schema_pedido.py
@@ -53,6 +53,9 @@ class ItemPedidoResponse(BaseModel):
     produto_imagem_snapshot: Optional[str] = None
     model_config = ConfigDict(from_attributes=True)
 
+class MeioPagMini(BaseModel):
+    id: int
+
 class PedidoResponse(BaseModel):
     id: int
     status: PedidoStatusEnum
@@ -60,7 +63,7 @@ class PedidoResponse(BaseModel):
     empresa_id: int
     entregador_id: Optional[int]
     endereco_id: Optional[int]
-    meio_pagamento: Optional[MeioPagamentoResponse] = None
+    meio_pagamento: Optional[MeioPagMini] = None
     tipo_entrega: TipoEntregaEnum
     origem: OrigemPedidoEnum
     subtotal: float
