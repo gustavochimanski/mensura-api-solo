ede9d74495c0aacb399175e195c61ce6d3423eef
Gustavo chimanski antunes <gustavochimanski@outlook.com.br>
2025-10-12 15:40:35 -0300

    .

--- app/api/mensura/router/router.py
+++ app/api/mensura/router/router.py
@@ -2,18 +2,32 @@
 
 from fastapi import APIRouter, Depends
 
-from app.api.mensura.router.router_empresa import router as router_empresa
-from app.api.mensura.router.router_empresa_client import router as router_empresa_client
+from app.api.mensura.router.empresa.router_empresa_admin import (
+    router as router_empresa_admin,
+)
+from app.api.mensura.router.empresa.router_empresa_public import (
+    router as router_empresa_public,
+)
 from app.api.mensura.router.router_usuario import router as router_usuario
-from app.api.mensura.router.router_geo_api_fy import router as router_geoapify
-from app.api.mensura.router.router_endereco import router as router_endereco
+from app.api.mensura.router.geoapify.router_geoapify_client import (
+    router as router_geoapify_client,
+)
+from app.api.mensura.router.geoapify.router_geoapify_admin import (
+    router as router_geoapify_admin,
+)
+from app.api.mensura.router.router_endereco_admin import (
+    router as router_endereco_admin,
+)
 from app.api.mensura.router.router_produtos import router as router_produtos
 from app.core.admin_dependencies import get_current_user
 
 mensura_router = APIRouter()
-mensura_router.include_router(router_empresa, dependencies=[Depends(get_current_user)])
-mensura_router.include_router(router_empresa_client)
-mensura_router.include_router(router_endereco, dependencies=[Depends(get_current_user)])
-mensura_router.include_router(router_geoapify)
+mensura_router.include_router(router_empresa_admin, dependencies=[Depends(get_current_user)])
+mensura_router.include_router(router_empresa_public)
+mensura_router.include_router(
+    router_endereco_admin, dependencies=[Depends(get_current_user)]
+)
+mensura_router.include_router(router_geoapify_client)
+mensura_router.include_router(router_geoapify_admin)
 mensura_router.include_router(router_produtos, dependencies=[Depends(get_current_user)])
 mensura_router.include_router(router_usuario, dependencies=[Depends(get_current_user)])
