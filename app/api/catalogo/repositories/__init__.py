da39c6fcb3196145b106501c394fd493f37a7d17
gustavochimanski <gustavo@gtechtecnologia.com.br>
2025-11-18 15:21:56 -0300

    .

--- app/api/catalogo/repositories/__init__.py
+++ app/api/catalogo/repositories/__init__.py
@@ -1,14 +1,12 @@
 from .repo_produto import ProdutoMensuraRepository
 from .repo_combo import ComboRepository
 from .repo_adicional import AdicionalRepository
-from .repo_ingrediente import IngredienteRepository
 from .repo_receitas import ReceitasRepository
 
 __all__ = [
     "ProdutoMensuraRepository",
     "ComboRepository",
     "AdicionalRepository",
-    "IngredienteRepository",
     "ReceitasRepository",
 ]
 
