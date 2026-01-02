8c5308f433a54e3628868dc0f865c4cff3e19d5e
gustavochimanski <gustavo@gtechtecnologia.com.br>
2026-01-02 16:58:52 -0300

    .

--- app/api/caixas/router/router.py
+++ app/api/caixas/router/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.api.caixas.router.router_caixa_crud import router as router_caixa_crud
 from app.api.caixas.router.router_caixa_abertura import router as router_caixa_abertura
+from app.api.caixas.router.router_caixa_admin import router as router_caixa_admin
 
 # Router principal que agrupa todos os routers de caixa
 router = APIRouter(
@@ -11,4 +12,5 @@ router = APIRouter(
 # Inclui todos os sub-routers
 router.include_router(router_caixa_crud)
 router.include_router(router_caixa_abertura)
+router.include_router(router_caixa_admin)
 
